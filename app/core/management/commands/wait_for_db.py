ccb9bbab9827bcf36c8857ce6921026b72401385
Christian <nshimyumukizachristian>
2020-11-22 13:43:43 +0200

    Added Endpoint for creating and authenticating Users

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -18,6 +18,6 @@ class Command(BaseCommand):
                 db_conn = connections["default"]
             except OperationalError:
                 self.stdout.write(
-                    "Databasa unavailable , waiting 1 second ...")
+                    "Database unavailable , waiting 1 second ...")
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database Available'))
